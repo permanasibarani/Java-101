d6cdee29dabc2ee00139b55d90e49acfc683303d
Permana Sibarani <permanakaisarmiraclesibarani@gmail.com>
2024-11-24 22:29:34 +0700

    Update ValidationService.java

--- Basic-OOP/src/Service/ValidationService.java
+++ Basic-OOP/src/Service/ValidationService.java
@@ -1,11 +1,10 @@
 package Service;
 
 import Card.AbstractCard;
-
-public class ValidationService{
-    public static void validate(AbstractCard card) {
-        if(card.getCardQuestion() == null){
-            throw new NullPointerException("Pertanyaan tidak boleh kosong");
+            public class ValidationService{
+                public static void validate(AbstractCard card) {
+                    if(card.getCardQuestion() == null){
+                        throw new NullPointerException("Pertanyaan tidak boleh kosong");
         }
     }
 }
