47979e508b84018c5db3f3b01a7bd77fd0e145dc
Permana Sibarani <permanakaisarmiraclesibarani@gmail.com>
2024-11-26 23:42:29 +0700

    feat: use ArrayList

--- Basic-OOP/src/App/CardApp.java
+++ Basic-OOP/src/App/CardApp.java
@@ -6,6 +6,7 @@ import Card.CardEnum;
 import Card.ChoicesCard;
 import Service.ValidationService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,10 +14,8 @@ public class CardApp {
     public static void main(String[] args){
         ChoicesCard card = new ChoicesCard("Foo?", "Bar", CardEnum.ChoicesCard);
 
-        card.setCardChoices(new String[]{"Wow", "Wew", "Wuw"});
-        System.out.println(card.getStringCardChoices());
-        System.out.println(card.stringChoices());
-        System.out.println(card.getCardAnswer());
-        System.out.println(Arrays.toString(card.getCardChoices()));
+        List <String> choices = Arrays.asList("Foo", "Bar", "Nor");
+        card.setInitialChoices(choices);
+        System.out.println(card.getCardChoices());
     }
 }
--- Basic-OOP/src/Card/ChoicesCard.java
+++ Basic-OOP/src/Card/ChoicesCard.java
@@ -1,42 +1,23 @@
 package Card;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ChoicesCard extends AbstractCard {
-    private String[] cardChoices;
+    private List<String> cardChoices;
 
     public ChoicesCard(String cardQuestion, String cardAnswer, CardEnum cardType) {
         super(cardQuestion, cardAnswer, cardType);
+        this.cardChoices = new ArrayList<>();
     }
 
-    public String[] getCardChoices() {
-        return cardChoices;
+    public List<String> getCardChoices(){
+        return this.cardChoices;
     }
 
-    public void setCardChoices(String[] cardChoices) {
-        this.cardChoices = cardChoices;
-    }
-
-    public String getStringCardChoices(){
-        Iterable<String> card = List.of(this.cardChoices);
-
-        StringBuilder cardList = new StringBuilder();
-
-        for (var item:card){
-            cardList.append(item);
-        }
-        return cardList.toString();
-    }
-
-    public String stringChoices(){
-
-        List<String> card = new ArrayList<>();
-
-        for(var i =0; i < this.cardChoices.length; i++){
-            card.add(this.cardChoices[i]);
-        }
-        card.removeLast();
-        return card.toString();
+    public void setInitialChoices(List<String> choices){
+        this.cardChoices.addAll(choices);
     }
 }
